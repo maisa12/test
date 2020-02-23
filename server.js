4641135bcee2dd3a0631a3db7d15af60b94038a0
maisa <nikamaisa@gmail.com>
2020-02-23 12:45:26 +0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 8000;
 
 app.get('/',(req,res)=>{
-  res.sendFile('./index.html',{root: __dirname})
+  res.sendFile('./templates/index.html',{root: __dirname})
 })
 app.use('/static', express.static(__dirname+'/static'));
+app.use('/templates', express.static(__dirname+'/templates'));
 app.listen(port, ()=>console.log(`Server is listening: ${port}`))
