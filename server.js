347679daa024766bca2def790211abe26bfbf177
maisa <nikamaisa@gmail.com>
2020-03-05 21:34:38 +0400

    add endpoint

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const port = 8000;
 var bodyParser = require('body-parser');
 var id = 0;
 var todoList = [];
+const papers =[
+  {
+      name: 'Paper N1',
+      id:1,
+      notes: [{value:"test1", id:0, done: true, check:false}, {value:"test2", id:1, done: false, check:false}]
+  },
+  {
+      name: 'Paper N2',
+      id:2,
+      notes: [{value:"test1", id:0, done: true, check:false}, {value:"test2", id:1, done: false, check:false}]
+  }
+] 
 var jsonParser = bodyParser.json();
 app.get('/',(req,res)=>{
   res.sendFile('./templates/index.html',{root: __dirname})
@@ -15,6 +27,20 @@ app.get('/getpost',(req,res)=>{
 app.post('/post', jsonParser, function (req, res) {
   todoList.push({id: id, value: req.body.notes, done: false})
   id++;
+})
+app.get('/papers',(req,res)=>{
+  var array = [];
+  papers.map(x=>array.push(x.name))
+  res.send(array)
+})
+app.get('/papers/:id',(req,res)=>{
+  var note = papers.filter(x=>x.id===Number(req.params.id));
+  if(note.length===0){
+    res.send("This paper doesn't exist")
+  }
+  else{
+  res.send(note[0].notes)
+  }
 })
 app.post('/delete', jsonParser, function (req, res) {
  todoList = todoList.filter(x=>req.body.delete.indexOf(x.id.toString())===-1)
